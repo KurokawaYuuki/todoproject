b1774ef5fd169a88db4de4ca3bd4bbe16976b262
”KurokawaYuuki” <koara@MacBook-Pro.local>
2020-10-19 20:30:03 +0900

    chapter.3

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $id,
+        ]);
+    }
+}
